9201e3544b4f0af0ef75a6263994b4385e203a54
Timofey Peshin <timoffex@gmail.com>
2018-11-07 11:20:36 -0800

    Reorganized GameManager files
    Created a GameManager subdirectory under Scripts. Moved the different
    state handlers to their own files inside a GameManager/States
    subdirectory.

---
+++ Everything is Temporary/Assets/Scripts/GameManager/States/ShowingBookHandler.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+
+public partial class GameManager : MonoBehaviour
+{
+    private class ShowingBookHandler : StateHandler
+    {
+        public ShowingBookHandler(GameManager gameManager) : base(gameManager)
+        {
+        }
+
+        public override State GetState()
+        {
+            return State.ShowingBook;
+        }
+
+        protected override async Task<StateHandler> SwitchTo_Impl(State state)
+        {
+            switch (state)
+            {
+                case State.ShowingTown:
+                    return await SwitchToTown();
+
+                default:
+                    return null;
+            }
+        }
+
+        private async Task<StateHandler> SwitchToTown()
+        {
+            GameManager gm = GameManagerParent;
+
+            // Can only switch to town view if a town is loaded.
+            if (gm.m_loadedTown == null)
+                return null;
+
+            // Since a town is loaded, there must be a town camera registered.
+            Debug.Assert(gm.m_townCamera != null);
+
+            // Disable mouse inputs to pages.
+            // TODO: Disable MouseInput too.
+            gm.m_townInputBlocker = gm.m_pageInputModule.IgnoreInput();
+
+            // Fade out main camera.
+            {
+                bool success = false;
+                SemaphoreSlim fadeSema = new SemaphoreSlim(0);
+
+                gm.FadeCamera(gm.BookCamera, 0, 1, 1, Color.black, () =>
+                {
+                    success = true;
+                    fadeSema.Release();
+                }, () =>
+                {
+                    success = false;
+                    fadeSema.Release();
+                });
+
+                await fadeSema.WaitAsync();
+
+                // If fading was canceled, bail out!
+                if (!success)
+                {
+                    gm.m_townInputBlocker.Unsubscribe();
+                    gm.m_townInputBlocker = null;
+                    return null;
+                }
+            }
+
+            // Begin fade in town camera.
+            gm.FadeCamera(gm.m_townCamera, 1, 0, 1, Color.black);
+
+            gm.m_townCamera.enabled = true;
+            gm.BookCamera.enabled = false;
+
+            // Enable town event system.
+            gm.BookEventSystem.enabled = false;
+            gm.TownEventSystem.enabled = true;
+
+            return new ShowingTownHandler(gm);
+        }
+    }
+}
---
+++ Everything is Temporary/Assets/Scripts/GameManager/States/ShowingTownHandler.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+
+public partial class GameManager : MonoBehaviour
+{
+    private class ShowingTownHandler : StateHandler
+    {
+        public ShowingTownHandler(GameManager gameManager) : base(gameManager)
+        {
+        }
+
+        public override State GetState()
+        {
+            return State.ShowingTown;
+        }
+
+        protected override async Task<StateHandler> SwitchTo_Impl(State state)
+        {
+            switch (state)
+            {
+                case State.ShowingBook:
+                    return await SwitchToBook();
+                default:
+                    return null;
+            }
+        }
+
+        private async Task<StateHandler> SwitchToBook()
+        {
+            GameManager gm = GameManagerParent;
+
+            // There must be a town camera in this state.
+            Debug.Assert(gm.m_townCamera != null);
+
+            // There must similarly be an input blocker token.
+            Debug.Assert(gm.m_townInputBlocker != null);
+
+            // Fade town camera.
+            {
+                bool success = false;
+                SemaphoreSlim sema = new SemaphoreSlim(0);
+
+                gm.FadeCamera(gm.m_townCamera, 0, 1, 1, Color.black, () =>
+                {
+                    success = true;
+                    sema.Release();
+                }, () =>
+                {
+                    success = false;
+                    sema.Release();
+                });
+
+                await sema.WaitAsync();
+
+                // If fading is canceled, bail.
+                if (!success)
+                    return null;
+            }
+
+            // Begin fading in book camera.
+            gm.FadeCamera(gm.BookCamera, 1, 0, 1, Color.black);
+
+            gm.BookCamera.enabled = true;
+            gm.m_townCamera.enabled = false;
+
+            // Enable book event system.
+            gm.m_townInputBlocker.Unsubscribe();
+            gm.m_townInputBlocker = null;
+
+            gm.TownEventSystem.enabled = false;
+            gm.BookEventSystem.enabled = true;
+
+            return new ShowingBookHandler(gm);
+        }
+    }
+}
