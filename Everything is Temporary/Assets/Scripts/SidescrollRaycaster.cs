4a4d0c2ca89c59d9e102e042e5a3f177bd3a60cd
Timofey Peshin <timoffex@gmail.com>
2018-11-08 20:59:57 -0800

    Added input-forwarding to FirstTown
    The SidescrollRaycaster class captures mouse input that is sent to the
    sidescroller display quad and forwards it to a different raycaster.

---
+++ Everything is Temporary/Assets/Scripts/SidescrollRaycaster.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class SidescrollRaycaster : BaseRaycaster
+{
+    public override Camera eventCamera => m_delegateRaycaster.eventCamera;
+
+    protected override void Awake()
+    {
+        m_delegateRaycaster.enabled = false;
+    }
+
+    public override void Raycast(PointerEventData eventData, List<RaycastResult> resultAppendList)
+    {
+        Vector2? normalizedCoords = MainRaycastHelper.Singleton.RaycastScrollview(eventData.position);
+
+        if (!normalizedCoords.HasValue)
+            return;
+
+        Vector2 oldPosition = eventData.position;
+
+        eventData.position = Vector2.Scale(normalizedCoords.Value, eventCamera.pixelRect.size);
+        m_delegateRaycaster.Raycast(eventData, resultAppendList);
+        eventData.position = oldPosition;
+    }
+
+    [SerializeField]
+    [Tooltip("The sidescroll raycaster will forward mouse inputs from the" +
+             " sidescrolling quad to this raycaster.")]
+    private BaseRaycaster m_delegateRaycaster = null;
+}
---
+++ Everything is Temporary/Assets/Scripts/TestEvents.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TestEvents : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        Debug.LogFormat("It worked! I am {0}.", name);
+    }
+}
