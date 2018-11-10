bdc6708bcbfce4375379c5efe5af71d261c430cb
Reshma Z <nomnomreshma@gmail.com>
2018-11-10 00:39:17 -0800

    Added sprites and animation for main character
    Mainly, added Player sprite and walking animation. The animation slows
    with player movement speed from player Input.GetAxis (xMovement) so I
    made use of Freeman’s code.
    
    The animation controller and animation are stored under Images >
    Animations, where animations (.anim and .controller files) will be
    stored henceforth.
    
    I also updated the SidescrollerTexture.renderTexture max Width & height
    because it seems like this will need some tweaking to prevent warping
    sprites.
    
    Updates are accessible in game by going to Main Menu > Load Reshma Town

--- Everything is Temporary/Assets/Scripts/Player.cs
+++ Everything is Temporary/Assets/Scripts/Player.cs
@@ -6,7 +6,12 @@ public class Player : MonoBehaviour {
 	
 	[Range(0f, 0.1f)] public float movementSpeed;
 
-	private void Start()
+    private void Awake()
+    {
+        m_animator = GetComponent<Animator>();
+    }
+
+    private void Start()
 	{
 		
 	}
@@ -14,8 +19,11 @@ public class Player : MonoBehaviour {
 	private void Update()
 	{
 		float xMovement = Input.GetAxis("Horizontal");
-		
+
+        m_animator.SetFloat("MovementSpeed", xMovement);
+
 		transform.Translate(xMovement * movementSpeed, 0, 0);
 	}
 
+    private Animator m_animator;
 }
