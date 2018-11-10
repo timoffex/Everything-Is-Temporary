bd6da3ebcc348915ca29a128a42dad1682ab0a0b
Glowing Screen <override@keemail.me>
2018-11-09 22:47:16 -0800

    Player can move now. Camera can track player in a coroutine, with a delayed start.

---
+++ Everything is Temporary/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+	
+	[Range(0f, 0.1f)] public float movementSpeed;
+
+	private void Start()
+	{
+		
+	}
+	
+	private void Update()
+	{
+		float xMovement = Input.GetAxis("Horizontal");
+		
+		transform.Translate(xMovement * movementSpeed, 0, 0);
+	}
+
+}
