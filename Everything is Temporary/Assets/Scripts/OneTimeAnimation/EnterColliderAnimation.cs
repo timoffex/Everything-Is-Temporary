0ee971621a999e9b298b0a9ec32f1fe16c467cac
Glowing Screen <override@keemail.me>
2018-11-14 04:46:33 -0800

    One-time animation script for when the town loads and when the player enters the trigger are coded.

---
+++ Everything is Temporary/Assets/Scripts/OneTimeAnimation/EnterColliderAnimation.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnterColliderAnimation : MonoBehaviour {
+	
+	private void Start()
+	{
+		m_animator = GetComponent<Animator>();
+	}
+	
+	private void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.name == "Player")
+		{
+			//m_animator.SetTrigger("");
+		}
+		
+		enabled = false;
+	}
+	
+	private Animator m_animator;
+	
+}
---
+++ Everything is Temporary/Assets/Scripts/OneTimeAnimation/TownLoadAnimation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TownLoadAnimation : MonoBehaviour {
+	
+	// This should be called when the town loads.
+	private void Start()
+	{
+		m_animator = GetComponent<Animator>();
+		
+		//m_animator.SetTrigger("");
+		
+		enabled = false;
+	}
+	
+	private Animator m_animator;
+	
+}
