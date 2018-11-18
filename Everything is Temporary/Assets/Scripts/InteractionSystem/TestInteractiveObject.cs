d1706bf67db277d60ef766136e9cb603438691f9
Timofey Peshin <timoffex@gmail.com>
2018-11-17 17:46:50 -0800

    Speech bubble property in TestInteractiveObject

--- Everything is Temporary/Assets/Scripts/InteractionSystem/TestInteractiveObject.cs
+++ Everything is Temporary/Assets/Scripts/InteractionSystem/TestInteractiveObject.cs
@@ -16,18 +16,29 @@ public class TestInteractiveObject : MonoBehaviour, IInteractiveObject
     public void ShowSelectionVisual()
     {
         Debug.LogFormat("SHOW Visual for {0}.", gameObject.name);
+
+        m_speechBubble?.SetActive(true);
     }
 
     public void HideSelectionVisual()
     {
         Debug.LogFormat("HIDE Visual for {0}.", gameObject.name);
+
+        m_speechBubble?.SetActive(false);
     }
 
     private void Awake()
     {
         // Important to keep the return value alive!
-        m_unsubscriber = InteractionSystem.Singleton.RegisterInteractiveObject(this);
+        m_interactionSystemUnsubscriber = InteractionSystem.Singleton.RegisterInteractiveObject(this);
+
+        m_speechBubble?.SetActive(false);
     }
 
-    private IUnsubscriber m_unsubscriber;
+    private IUnsubscriber m_interactionSystemUnsubscriber;
+
+    [SerializeField]
+    [Tooltip("An object that should be enabled/disabled on ShowSelectionVisual()" +
+             " and ShowSelectionVisual().")]
+    private GameObject m_speechBubble;
 }
